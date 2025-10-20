a19fb0c1d8341aa6084a5c5d73ed8703d551f262
Angel Luis Pérez Jiménez <aperezj2122@politecnics.barcelona>
2025-10-20 19:03:15 +0200

    .
    Modificación de css para tenerlo claro, Eliminar un controlador en desuso

--- app/Http/Controllers/ProyectosController.php
+++ app/Http/Controllers/ProyectosController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ProyectosController extends Controller
 {
 
-            function index()  {
+            function proyecto()  {
                 return view("proyectos.proyecto");
 
 }}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,27 @@
 <?php
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TareasController;
+use App\Http\Controllers\TareaController;
+use App\Http\Controllers\ProyectosController;
 
 Route::get('/', function () {
     return view('landing');
 })->name('landing');
 /// aixhJBQDQW UV WCH WWC CQ LQJKDF WE WFLWE WELFLFLFLFLFFLFLFLFLFLFLFLFLLLLL
 
-Route::get('/index', [TareasController::class, 'index']);
+Route::get('/index', [TareaController::class, 'index']);
 
+Route::get('/proyecto', [ProyectosController::class, 'proyecto']);
+
+
+//Web page Tareas
 Route::get('/max', function (){
     return view('max');
 });//hgukvgvgj
+
+
+//Web Page Proyecto
+Route::get('/proyecto', function(){
+    return view('proyecto');
+}) -> name('proyecto');
