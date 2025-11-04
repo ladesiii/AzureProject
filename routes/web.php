868c8f13df9c715bfec739f2936e77480551e372
Max-Vidal <mvidaln2425@politecnics.barcelona>
2025-11-04 16:49:40 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,9 @@ Route::get('/tareas', [TareaController::class, 'index'])->name('tareas');
 Route::get('/tareas', function (){
     return view('tareas');
 
-<<<<<<< HEAD
+
 })->name('tareas');//hgukvgvgj
 Route::get('/tareas', [TareaController::class, 'tareas'])->name('tareas');
-//hgukvgvgj
-=======
-});//hgukvgvgj
->>>>>>> parent of f7b3fe8 (Merge pull request #36 from ladesiii/Angel)
-
-
 
 //Web Page Proyecto (handled by controller)
 Route::get('/proyecto', [ProyectosController::class, 'proyecto'])->name('proyecto');
