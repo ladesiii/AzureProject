9d9c2a2819ed013cc52c3dd0028b79fa1512f5e7
Angel Luis Pérez Jiménez <aperezj2122@politecnics.barcelona>
2025-11-14 17:32:11 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TareaController;
-<<<<<<< HEAD
-use App\Http\Controllers\registroController;
-=======
-use App\Http\Controllers\UsuarioController;
->>>>>>> main
 use App\Http\Controllers\ProyectosController;
+use App\Http\Controllers\TareaController;
+use App\Http\Controllers\UsuarioController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
@@ -17,28 +12,23 @@ Route::get('/', function () {
 // Web page Tareas
 Route::get('/tareas', [TareaController::class, 'tareas'])->name('tareas');
 
-
 //Pagina de Proyecto
 Route::get('/proyecto', [ProyectosController::class, 'proyecto'])->name('proyecto');
 
 //Endpoint para crear proyecto (POST)
 Route::post('/proyecto', [ProyectosController::class, 'store'])->name('proyecto.store');
 
-
 Route::get('/login', [UsuarioController::class, 'login'])->name('login');
-
-
 
 // Registro view (simple GET route)
 Route::get('/registro', function () {
     return view('registro');
 })->name('registro');
 
-<<<<<<< HEAD
+
 //Registro
 Route::get('/registro', [registroController::class, 'registro'])->name('registro');
 
 Route::view('/login', 'login')->name('login');
 
-=======
->>>>>>> main
+
